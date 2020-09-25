8c2515a1721026b8ba003e58fd9946914d77f5cc
FocacciaSyin <simoncat524@hotmail.com>
2020-09-25 22:41:41 +0800

    此版本確定可以顯示結果

---
+++ APIStockQ/Context/StockQContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using APIStockQ;
+using APIStockQ.Model;
+
+namespace APIStockQ.Context
+{
+    public class StockQContext : DbContext
+    {
+        public StockQContext(DbContextOptions<StockQContext> options) : base(options) { }
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //    => options.UseSqlite("Data Source=StockQ.db");
+        //public DbSet<Home> Homes { get; set; }
+        //public DbSet<Markets> Markets { get; set; }
+
+        public DbSet<WeatherForecast> WeatherForecast { get; set; }
+    }
+}
